f26c7489d56841c95d9c5e15e9daa8c35fa72e30
Gregoire FRANCOIS <gregoire.p.francois@gmail.com>
2025-10-13 10:38:14 -0400

    FIX : Favicon

--- gregoire-portfolio/src/lib/seo.ts
+++ gregoire-portfolio/src/lib/seo.ts
@@ -8,7 +8,7 @@ export const defaultMetadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: '/GF-logo.jpg',
+    icon: '/GF-logo.ico',
   },
   keywords: [
     'freelance',
